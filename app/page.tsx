fc113abd31953031a02b885c58d6e7c7b0da53a4
trashpirate <nadinaoates@gmail.com>
2023-10-10 14:04:28 -0500

    updating favicon

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,10 @@ import CopyToClipboard from "@/components/copyToClipboard";
 import Link from "next/link";
 import TokenInfo from "@/components/tokenInfo";
 import ReflectionChecker from "@/components/reflectionChecker";
-import AnimatedCursor from "react-animated-cursor";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={8}
-        color="193, 11, 111"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-        ]}
-      />
       <div className={styles.container}>
         <h1>BUY, HOLD, EARN</h1>
         <div className={styles.description}></div>
