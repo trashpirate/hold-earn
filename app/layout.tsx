f6c95066de73ecd0a33d2dd892ec3c5a14d87082
trashpirate <nadinaoates@gmail.com>
2023-10-10 14:07:19 -0500

    updating favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <title>Plot Minter</title>
+      <title>BUY HOLD EARN BURN</title>
       <meta name="description" content="Buy, Hold, Earn, Burn" key="desc" />
       <meta name="twitter:card" content="summary_large_image" key="twcard" />
       <meta name="twitter:creator" content="buyholdearn" key="twhandle" />
