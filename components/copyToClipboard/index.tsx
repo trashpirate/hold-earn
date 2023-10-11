59866f7d771a54fbf8870452e180f23fd7b63402
trashpirate <nadinaoates@gmail.com>
2023-10-11 12:26:52 -0500

    adding copied notification

--- components/copyToClipboard/index.tsx
+++ components/copyToClipboard/index.tsx
@@ -1,11 +1,15 @@
 "use client";
 import styles from "./copytoclipboard.module.css";
 import { BiCopy } from "react-icons/bi";
+import { useState } from "react";
 
 export default function CopyToClipboard(props: { text: string; copyText: string }) {
+  const [copied, setCopied] = useState<boolean>(false);
+
   const copylink = async (e: any) => {
     try {
       navigator.clipboard.writeText(props.copyText);
+      setCopied(true);
       console.log("copied: " + props.copyText);
     } catch (e) {
       console.log(e);
@@ -16,9 +20,12 @@ export default function CopyToClipboard(props: { text: string; copyText: string 
     <div className={styles.container}>
       <h3>{props.text}</h3>
       <div onClick={copylink} className={styles.copy}>
-        <BiCopy />
+        <div className={styles.copy_icon}>
+          <BiCopy />
+        </div>
+
+        {copied && <div className={styles.notification}>Copied!</div>}
       </div>
     </div>
   );
-
 }
