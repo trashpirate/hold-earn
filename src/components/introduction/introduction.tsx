c597ec7e969b60bbe6691af2921c6711a16886dc
trashpirate <nadinaoates@gmail.com>
2023-12-27 10:27:19 -0600

    adding flameling dialog

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -5,10 +5,21 @@ import Link from 'next/link';
 import ChartButton from '../buttons/chartButton';
 import CopyToClipboard from '../copyToClipboard';
 import NftButton from '../buttons/nftButton';
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
 type Props = {};
 
-export default function Introduction({}: Props) {
 
+export default function Introduction({}: Props) {
+  let [isOpen, setIsOpen] = useState(true)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
   return (
     <section id="introduction" className='px-7 my-16 scroll-m-36'>
       <div className=" sm:bg-center h-auto w-full grid lg:grid-cols-2 items-center max-w-7xl mx-auto border border-primary mt-24 rounded-2xl relative justify-between">
@@ -90,7 +101,84 @@ export default function Introduction({}: Props) {
   
         </div>
       </div>
+        <>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={openModal}
+          className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none hover:ring-2 hover:ring-white/75"
+        >
+          Open dialog
+        </button>
+      </div>
 
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white/5 backdrop-blur p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-2xl font-medium leading-2 text-primary"
+                  >
+                    FLAMLING COLLECTION LIVE NOW!
+                    </Dialog.Title>
+                    <div className='w-full flex justify-center h-fit'>
+                      <video autoPlay muted loop style={{ borderRadius: "25px", margin: "20px" }}>
+        <source src="/flameling_moving.mp4" />
+      </video>
+            
+          </div>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-200">
+                      The adorable Flamelings embody the warmth and energy of the fire they are born from. Funny, powerful, brave, patient, and dependable, they reflect the diverse qualities of the EARN community.
+                    </p>
+                  </div>
+
+                  <div className="mt-4 gap-4 flex">
+                    <Link
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-black hover:bg-orange-800 focus:outline-none hover:ring-2 hover:ring-orange-500 hover:ring-offset-2"
+                      href="https://flame.buyholdearn.com/"
+                    >
+                      MINT
+                      </Link>
+                      <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-black hover:bg-orange-800 focus:outline-none hover:ring-2 hover:ring-orange-500 hover:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
     </section>
 
   );
