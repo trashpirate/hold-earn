9966371c94d7e3b819d3eb533a075a06c03240ee
trashpirate <nadinaoates@gmail.com>
2023-12-27 11:53:33 -0600

    fix homepage

--- src/components/introduction/popup.tsx
+++ src/components/introduction/popup.tsx
@@ -21,17 +21,6 @@ export default function PopUp({}: Props) {
 
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          {/* <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black/25" />
-          </Transition.Child> */}
 
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
@@ -57,7 +46,8 @@ export default function PopUp({}: Props) {
             width={300}
             height={300}
             style={{borderRadius: "25px", margin: "20px"}}
-            alt="flameling"
+                      alt="flameling"
+                      priority
           />
             
           </div>
