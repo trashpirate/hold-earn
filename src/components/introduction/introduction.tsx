d6682bb797f1914be0e997cb60b5340fd123db17
trashpirate <nadinaoates@gmail.com>
2023-12-27 11:27:06 -0600

    remove open button

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -102,15 +102,7 @@ export default function Introduction({}: Props) {
         </div>
       </div>
         <>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <button
-          type="button"
-          onClick={openModal}
-          className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none hover:ring-2 hover:ring-white/75"
-        >
-          Open dialog
-        </button>
-      </div>
+      
 
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
