224a62233ff7a85156a62d548421daa463f4c1b9
trashpirate <nadinaoates@gmail.com>
2024-01-11 19:08:06 -0600

    fix footer

--- src/components/flamelings/footer/footer.tsx
+++ src/components/flamelings/footer/footer.tsx
@@ -12,7 +12,7 @@ export default function Footer({scan_link}: Props) {
     <footer className=' bg-white/5 backdrop-blur w-full z-10 h-fit flex justify-center'>
       <div className='flex justify-between p-8 max-w-7xl w-full flex-col xs:flex-row'>
 
-        <div className='my-4 flex flex-row w-full h-12 gap-5 align-middle '>
+          <Link href="https://buyholdearn.com" className='my-4 flex flex-row w-full h-12 gap-5 align-middle '>
           <Image
             className='w-12 h-auto'
             src="/earn_logo_round_transparent.png"
@@ -21,13 +21,13 @@ export default function Footer({scan_link}: Props) {
             alt="earn logo"
           />
           <h1 className='font-bold text-lg text-left align-middle my-auto'>BUY, HOLD, EARN</h1>
-        </div>
+          </Link>
         <div className='flex flex-col justify-between h-full xs:w-2/4 '>
           
-          <div className='my-4 flex gap-4 w-full justify-start xs:justify-end h-12'>
-            <SocialIcon network="telegram" url="https://t.me/buyholdearn" bgColor="#FF6B10aa" style={{height: 30, width: 30}} label="Telegram" />
-            <SocialIcon network="x" url="https://twitter.com/buyholdearn" bgColor="#FF6B10aa" style={{height: 30, width: 30}} label="X" />
-            <Link href={scan_link}>
+          <div className='my-4 flex gap-4 w-full justify-start xs:justify-end h-12 align-middle'>
+            <SocialIcon className='my-auto' network="telegram" url="https://t.me/buyholdearn" bgColor="#FF6B10aa" style={{height: 30, width: 30}} label="Telegram" />
+            <SocialIcon className='my-auto' network="x" url="https://twitter.com/buyholdearn" bgColor="#FF6B10aa" style={{height: 30, width: 30}} label="X" />
+            <Link href={scan_link} className='my-auto'>
               <Image
                 src="/etherscan.png"
                 width={122}
