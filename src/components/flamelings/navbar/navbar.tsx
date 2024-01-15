e44b87da485ee8ce01a54b6fb8be8bb979fd684b
trashpirate <nadinaoates@gmail.com>
2024-01-14 18:02:56 -0600

    generalized flameling page

--- src/components/flamelings/navbar/navbar.tsx
+++ src/components/flamelings/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 var classNames = require('classnames');
-import {Disclosure} from '@headlessui/react';
-import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 import BuyButton from './buyButton';
 import Link from 'next/link';
@@ -10,20 +10,15 @@ type Props = {
   ticker: string;
 }
 
-const navigation = [
-  {name: 'Introduction', href: '../#introduction', current: true},
-  {name: 'Tokenomics', href: '#tokenomics', current: false},
 
-];
-
-export default function Navbar({contract, ticker}: Props) {
+export default function Navbar({ contract, ticker }: Props) {
   return (
     <Disclosure as="nav" className="fixed top-0 bg-white/5 backdrop-blur w-full z-20">
-      {({open}) => (
+      {({ open }) => (
         <>
           <div className="mx-4 md:mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
             <div className="relative flex h-20 items-center justify-between ">
-              
+
               <div className="flex flex-1 items-center justify-between sm:items-stretch sm:justify-start align-middle">
                 <Link href="https://buyholdearn.com/" className="flex flex-shrink-0 items-center my-auto">
                   <Image
@@ -34,13 +29,13 @@ export default function Navbar({contract, ticker}: Props) {
                     alt="EARN"
                   />
                 </Link>
-                
+
               </div>
               <BuyButton contract={contract} ticker={ticker}></BuyButton>
             </div>
           </div>
 
-          
+
         </>
       )}
     </Disclosure>
