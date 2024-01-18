12ac1532db6d8efe446590df6fa92ef1a56644da
trashpirate <nadinaoates@gmail.com>
2024-01-18 08:15:10 -0600

    update token info

--- src/app/0x21/page.tsx
+++ src/app/0x21/page.tsx
@@ -10,12 +10,12 @@ import FlamelingIntroduction from "@/components/flamelings/introduction/introduc
 const NFT_ID = "21";
 const NFT_URL = "https://opensea.io/assets/ethereum/0x49902747796c2abcc5ea640648551ddbc2c50ba2/21";
 const IMAGE_URL = "/flamelings/0x21.gif"
-const CONTRACT = "Coming soon!";
+const CONTRACT = "0x3984e3807BC8ac705C9ED612Ad4BB7A7c4028f01";
 const TICKER = "0x21";
-const SCAN_LINK = "#"
-const CHART_LINK = "#"
+const SCAN_LINK = "https://bscscan.com/token/0x3984e3807BC8ac705C9ED612Ad4BB7A7c4028f01"
+const CHART_LINK = "https://www.dextools.io/app/en/bnb/pair-explorer/0x88386f86ee2a2248a7803029226818faf276e0bb"
 const TG_LINK = "https://t.me/FLAMELING0X21"
-const X_LINK = "https://twitter.com/buyholdearn"
+const X_LINK = "https://twitter.com/0X21Flameling"
 const HOLDER_PERCENTAGE = "3"
 
 export default function Home() {
