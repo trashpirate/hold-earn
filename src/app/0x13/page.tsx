9ea9842901e1b8f26f76e9e95127101577606184
trashpirate <nadinaoates@gmail.com>
2024-01-19 12:25:28 -0600

    updating intro with links

--- src/app/0x13/page.tsx
+++ src/app/0x13/page.tsx
@@ -14,6 +14,7 @@ const CONTRACT = "0x96261e8Ed04FfDB190125FC48649eF106dD57886";
 const TICKER = "0x13";
 const SCAN_LINK = "https://bscscan.com/address/0x96261e8Ed04FfDB190125FC48649eF106dD57886#codes"
 const CHART_LINK = "https://www.dextools.io/app/en/bnb/pair-explorer/0xd245f2b004fb095367376eba5cba4d7347a3cb74"
+const LOCK_LINK = "https://app.uncx.network/amm/pancake-v2/pair/0xd245f2b004fb095367376eba5cba4d7347a3cb74"
 const TG_LINK = "https://t.me/FLAMELING0X13"
 const X_LINK = "https://twitter.com/0x13flameling"
 const HOLDER_PERCENTAGE = "1"
@@ -24,7 +25,7 @@ export default function Home() {
   return (
     <section className={styles.main}>
       <Navbar contract={CONTRACT} ticker={TICKER}></Navbar>
-      <FlamelingIntroduction id={NFT_ID} holderPercentage={HOLDER_PERCENTAGE} contract={CONTRACT} nftUrl={NFT_URL} imageUrl={IMAGE_URL} scanLink={SCAN_LINK} chartLink={CHART_LINK} tgLink={TG_LINK} xLink={X_LINK}></FlamelingIntroduction>
+      <FlamelingIntroduction id={NFT_ID} holderPercentage={HOLDER_PERCENTAGE} contract={CONTRACT} nftUrl={NFT_URL} imageUrl={IMAGE_URL} scanLink={SCAN_LINK} chartLink={CHART_LINK} lockLink={LOCK_LINK} tgLink={TG_LINK} xLink={X_LINK}></FlamelingIntroduction>
       <Footer scanLink={SCAN_LINK} tgLink={TG_LINK} xLink={X_LINK}></Footer>
     </section>
   );
--- src/app/0x21/page.tsx
+++ src/app/0x21/page.tsx
@@ -14,6 +14,7 @@ const CONTRACT = "0x3984e3807BC8ac705C9ED612Ad4BB7A7c4028f01";
 const TICKER = "0x21";
 const SCAN_LINK = "https://bscscan.com/token/0x3984e3807BC8ac705C9ED612Ad4BB7A7c4028f01"
 const CHART_LINK = "https://www.dextools.io/app/en/bnb/pair-explorer/0x88386f86ee2a2248a7803029226818faf276e0bb"
+const LOCK_LINK = "#";
 const TG_LINK = "https://t.me/FLAMELING0X21"
 const X_LINK = "https://twitter.com/0X21Flameling"
 const HOLDER_PERCENTAGE = "3"
@@ -22,7 +23,7 @@ export default function Home() {
   return (
     <section className={styles.main}>
       <Navbar contract={CONTRACT} ticker={TICKER}></Navbar>
-      <FlamelingIntroduction id={NFT_ID} holderPercentage={HOLDER_PERCENTAGE} contract={CONTRACT} nftUrl={NFT_URL} imageUrl={IMAGE_URL} scanLink={SCAN_LINK} chartLink={CHART_LINK} tgLink={TG_LINK} xLink={X_LINK}></FlamelingIntroduction>
+      <FlamelingIntroduction id={NFT_ID} holderPercentage={HOLDER_PERCENTAGE} contract={CONTRACT} nftUrl={NFT_URL} imageUrl={IMAGE_URL} scanLink={SCAN_LINK} chartLink={CHART_LINK} lockLink={LOCK_LINK} tgLink={TG_LINK} xLink={X_LINK}></FlamelingIntroduction>
       <Footer scanLink={SCAN_LINK} tgLink={TG_LINK} xLink={X_LINK}></Footer>
     </section>
   );
