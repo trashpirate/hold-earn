d9533c7f505ed734fede12e247d2a21f818eb556
trashpirate <nadinaoates@gmail.com>
2024-02-29 00:28:13 -0600

    fix circulating supply

--- src/app/api/circulating-supply/route.ts
+++ src/app/api/circulating-supply/route.ts
@@ -5,8 +5,16 @@ import { publicClient } from "../../../utils/client";
 import { abi } from "../../../assets/tokenABI";
 const TOKEN_ADDRESS = "0x0b61C4f33BCdEF83359ab97673Cb5961c6435F4E";
 
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
+ 
+const client = createPublicClient({ 
+  chain: mainnet, 
+  transport: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`) 
+}) 
+
 export async function GET() {
-  const data = await publicClient.readContract({
+  const data = await client.readContract({
     address: TOKEN_ADDRESS,
     abi: abi,
     functionName: "balanceOf",
@@ -14,6 +22,7 @@ export async function GET() {
   });
 
   const burned = Number(formatEther(data));
+  console.log(burned);
   const circulating_supply = 1000000000 - burned;
 
   return NextResponse.json(circulating_supply, { status: 200 });
