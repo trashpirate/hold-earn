11aa2672c6d8e8f3c1c4ebb1919612783e46e3c5
trashpirate <nadinaoates@gmail.com>
2024-03-01 15:29:55 -0600

    adding basechain buy button

--- src/components/navbar/buyButton.tsx
+++ src/components/navbar/buyButton.tsx
@@ -1,7 +1,7 @@
-import {Menu, Transition} from '@headlessui/react';
-import {PlusIcon} from '@heroicons/react/24/solid';
+import { Menu, Transition } from '@headlessui/react';
+import { PlusIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 var classNames = require('classnames');
 
 export default function BuyButton() {
@@ -30,7 +30,7 @@ export default function BuyButton() {
         >
           <Menu.Items className="absolute right-0 mt-2 w-64 origin-top-right rounded-md bg-orange-500/50 backdrop-blur-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <Menu.Item>
-              {({active}) => (
+              {({ active }) => (
                 <a
                   href="https://app.uniswap.org/swap?outputCurrency=0x0b61C4f33BCdEF83359ab97673Cb5961c6435F4E"
                   className={classNames(active ? 'bg-white/5' : '', 'block px-4 py-2 text-sm text-gray-300 font-bold')}
@@ -40,7 +40,7 @@ export default function BuyButton() {
               )}
             </Menu.Item>
             <Menu.Item>
-              {({active}) => (
+              {({ active }) => (
                 <a
                   href="https://pancakeswap.finance/swap?chain=bsc&outputCurrency=0x2aC895fEba458B42884DCbCB47D57e44c3a303c8"
                   className={classNames(active ? 'bg-white/5' : '', 'block px-4 py-2 text-sm text-gray-300 font-bold')}
@@ -50,7 +50,17 @@ export default function BuyButton() {
               )}
             </Menu.Item>
             <Menu.Item>
-              {({active}) => (
+              {({ active }) => (
+                <a
+                  href="https://pancakeswap.finance/swap?chain=base&outputCurrency=0x803b629C339941e2b77D2dC499DAc9e1fD9eAC66"
+                  className={classNames(active ? 'bg-white/5' : '', 'block px-4 py-2 text-sm text-gray-300 font-bold')}
+                >
+                  Buy on PancakeSwap (BASE)
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
                 <a
                   href="https://raydium.io/swap/?inputCurrency=sol&outputCurrency=8CuPzHKuZQUyYbear8fi3u6iuTY652WVZT2cHZ2yeNbE&fixed=in"
                   className={classNames(active ? 'bg-white/5' : '', 'block px-4 py-2 text-sm text-gray-300 font-bold')}
