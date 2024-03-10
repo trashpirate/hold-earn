d759efc36c0dbc4503635e84b6f3bd888c08d36b
trashpirate <nadinaoates@gmail.com>
2024-03-10 16:21:24 -0500

    fix cost in intro

--- src/components/flamelings/introduction/introduction.tsx
+++ src/components/flamelings/introduction/introduction.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import {SocialIcon} from 'react-social-icons';
+import { SocialIcon } from 'react-social-icons';
 import Link from 'next/link';
 import ChartButton from '../../buttons/chartButton';
 import CopyToClipboard from '../../copyToClipboard';
@@ -20,21 +20,21 @@ type Props = {
   xLink: string;
 };
 
-export default function FlamlingIntroduction({id, holderPercentage, contract, ticker, nftUrl, imageUrl, scanLink, chartLink, lockLink, tgLink, xLink}: Props) {
+export default function FlamlingIntroduction({ id, holderPercentage, contract, ticker, nftUrl, imageUrl, scanLink, chartLink, lockLink, tgLink, xLink }: Props) {
 
   return (
     <section className='px-7 my-8 scroll-m-24'>
       <div className=" sm:bg-center h-auto w-full flex flex-col-reverse md:grid md:grid-rows-1 md:grid-cols-2 items-center max-w-7xl mx-auto border border-primary my-24 rounded-2xl relative justify-between">
         <div className='my-8 md:my-16 mx-4 sm:mx-8 xl:ml-16 col-start-1 row-start-1 z-10 sm:max-w-2xl'>
           <div className='my-4 flex gap-4 w-full pl-1'>
-            <SocialIcon network="telegram" url={tgLink} bgColor="#FF6B10aa" style={{height: 25, width: 25}} label="Telegram" />
-            <SocialIcon network="x" url={xLink} bgColor="#FF6B10aa" style={{height: 25, width: 25}} label="X" />
+            <SocialIcon network="telegram" url={tgLink} bgColor="#FF6B10aa" style={{ height: 25, width: 25 }} label="Telegram" />
+            <SocialIcon network="x" url={xLink} bgColor="#FF6B10aa" style={{ height: 25, width: 25 }} label="X" />
             <Link href={scanLink}>
               <Image
                 src="/etherscan.png"
                 width={122}
                 height={122}
-                style={{width: "25px", height: "auto", opacity: "0.8"}}
+                style={{ width: "25px", height: "auto", opacity: "0.8" }}
                 alt="etherscan"
               />
             </Link>
@@ -43,15 +43,15 @@ export default function FlamlingIntroduction({id, holderPercentage, contract, ti
 
           <div className='w-full h-auto p-1'>
 
-            <h1 className='text-4xl font-bold mb-2'>{`FLAMELING ${ ticker }`}</h1>
+            <h1 className='text-4xl font-bold mb-2'>{`FLAMELING ${ticker}`}</h1>
             <h2 className='text-2xl leading-10'>{`A Flameling Dividend Token`}</h2>
             <p className='leading-7 mt-4 text-md'>
-              FLAMELING {ticker} is a BEP-20 dividend token associated with the <a className="text-primary hover:opacity-100 opacity-60 hover:underline underline-offset-2 font-medium" href={nftUrl}>FLAMELING NFT #{id}</a>. The dividends are collected by a 2% transaction tax and redistributed to holders in $EARN. An additional 2% tax is collected for initial launch costs (development, listing, initial liquidity, and operations - roughly $2K). The total supply is 1 Billion tokens while {holderPercentage}% of the supply is airdropped to the holder of <a className="text-primary hover:opacity-100 opacity-60 hover:underline underline-offset-2 font-medium" href={nftUrl}>FLAMELING NFT #{id}</a>. After launch, ownership is renounced and liquidity locked for 3 months on <a className="text-primary hover:opacity-100 opacity-60 hover:underline underline-offset-2 font-medium" href={lockLink}>UNCX</a>.
+              FLAMELING {ticker} is a BEP-20 dividend token associated with the <a className="text-primary hover:opacity-100 opacity-60 hover:underline underline-offset-2 font-medium" href={nftUrl}>FLAMELING NFT #{id}</a>. The dividends are collected by a 2% transaction tax and redistributed to holders in $EARN. An additional 2% tax is collected for initial launch costs (development, listing, initial liquidity, and operations). The total supply is 1 Billion tokens while {holderPercentage}% of the supply is airdropped to the holder of <a className="text-primary hover:opacity-100 opacity-60 hover:underline underline-offset-2 font-medium" href={nftUrl}>FLAMELING NFT #{id}</a>. After launch, ownership is renounced and liquidity locked for 3 months on <a className="text-primary hover:opacity-100 opacity-60 hover:underline underline-offset-2 font-medium" href={lockLink}>UNCX</a>.
             </p>
 
             <div className='mt-4'>
               <CopyToClipboard
-                text={`CA: ${ contract }`}
+                text={`CA: ${contract}`}
                 copyText={contract}
               />
             </div>
@@ -72,7 +72,7 @@ export default function FlamlingIntroduction({id, holderPercentage, contract, ti
               src={imageUrl}
               width={1024}
               height={1024}
-              style={{width: "100%", height: "auto", margin: ""}}
+              style={{ width: "100%", height: "auto", margin: "" }}
               alt="flameling"
               priority
             />
