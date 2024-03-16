b0e1f78266817837ffd8674999f038e923c050c7
trashpirate <nadinaoates@gmail.com>
2024-03-16 16:24:46 -0500

    add custom buttons for flamelings

--- src/components/buttons/customButton.tsx
+++ src/components/buttons/customButton.tsx
@@ -6,7 +6,7 @@ type Props = {
   name: string;
 };
 
-export default function NftButton({url, name} : Props) {
+export default function CustomButton({ url, name }: Props) {
   return (
     <div className="mt-4 w-fit h-10 px-4 py-2 flex items-center gap-2 bg-white/5 backdrop-blur rounded-md border-l-orange-500 border-l-2 justify-center hover:bg-white/10">
       <Link className="font-bold uppercase" href={url}>{name}</Link>
