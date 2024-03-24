73788e17524020c4d7b76ef0678bd4c728615ab3
trashpirate <nadinaoates@gmail.com>
2024-03-24 18:18:35 -0500

    minor fix

--- src/components/partnership/partnership.tsx
+++ src/components/partnership/partnership.tsx
@@ -21,18 +21,18 @@ export default function Partnership({ }: Props) {
                             priority
                         />
                     </Link>
-                    <div className='my-8 md:my-4 max-w-3xl mx-8'>
+                    <div className='my-8 md:my-4 md:max-w-3xl mx-4 md:mx-8'>
                         <h1 className='uppercase md:text-xl text-primary font-bold leading-6'>Are you an awesome crypto project and looking for partnerhsips?</h1>
                         <div className='flex flex-col justify-between'>
                             <div className='leading-6 my-4 text-sm md:text-lg text-white/50'>
                                 The EARN Partnerhsip Program elevates projects in the crypto space by bringing communitites together to share knowledge, support each other, and collaborate. Together, we will maximize the success and growth for everyone!
                             </div>
                             <div className='flex flex-row h-fit w-full justify-start align-middle'>
-                                <div className='leading-4 text-lg md:text-xl text-primary my-auto'>
+                                <div className='leading-6 text-lg md:text-xl text-primary my-auto'>
                                     Become our partner TODAY!
                                 </div>
                                 <Link className="flex flex-col justify-center align-middle" href="https://forms.gle/N1RFtESwo8ZeXZ2KA">
-                                    <ArrowTopRightOnSquareIcon className='h-6 mx-2 my-auto  text-primary duration-500 hover:scale-125 transform '></ArrowTopRightOnSquareIcon>
+                                    <ArrowTopRightOnSquareIcon className='h-6 mx-4 my-auto  text-primary duration-500 hover:scale-125 transform '></ArrowTopRightOnSquareIcon>
                                 </Link>
 
 
