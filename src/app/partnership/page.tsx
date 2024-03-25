e98df3b13a1920759ead64eea70bab39529f4971
trashpirate <nadinaoates@gmail.com>
2024-03-24 22:17:46 -0500

    fix form link

--- src/app/partnership/page.tsx
+++ src/app/partnership/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
                         <li className="leading-7">Projects must post weekly updates about the $EARN partnership on X tagging <a href="https://twitter.com/buyholdearn" className="text-primary/70 hover:text-primary">@buyholdearn</a> to help spread the word.</li>
                     </ol>
                     <div className='flex flex-row w-full justify-center align-middle mt-8 mb-4'>
-                        <Link href="https://forms.gle/N1RFtESwo8ZeXZ2KA" className='leading-6 text-md md:text-lg text-black bg-primary px-4 py-2 cursor-pointer rounded-xl font-bold my-auto duration-500 hover:scale-110 transform'>
+                        <Link href="https://forms.gle/tdLTVhWPGAES97nL9" className='leading-6 text-md md:text-lg text-black bg-primary px-4 py-2 cursor-pointer rounded-xl font-bold my-auto duration-500 hover:scale-110 transform'>
                             Become our partner TODAY!
                         </Link>
                         {/* <Link className="flex flex-col justify-center align-middle " >
