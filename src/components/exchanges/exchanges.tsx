36cb8cf6cde9f572748f773f685c303d911dfba0
trashpirate <nadinaoates@gmail.com>
2024-04-09 16:57:42 -0500

    adding trader joe and coinmarketcap

--- src/components/exchanges/exchanges.tsx
+++ src/components/exchanges/exchanges.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 type Props = {};
 
-export default function Exchanges({}: Props) {
+export default function Exchanges({ }: Props) {
     return (
         <section id="exchanges" className='mt-36 px-7 flex justify-center scroll-m-36'>
             <div className='max-w-7xl w-full flex justify-center flex-col'>
@@ -12,7 +12,7 @@ export default function Exchanges({}: Props) {
                     <h1 className='text-4xl font-bold'>EXCHANGES</h1>
                 </div>
                 <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-orange-500 to-transparent opacity-100 animate-pulse" />
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-16 my-16 justify-between align-top'>
+                <div className='grid grid-cols-1 lg:grid-cols-4 gap-8 my-16 justify-between align-top'>
 
                     <Link className="mt-0 mb-auto mx-auto" href="https://app.uniswap.org/swap?outputCurrency=0x0b61C4f33BCdEF83359ab97673Cb5961c6435F4E">
                         <Image
@@ -45,6 +45,16 @@ export default function Exchanges({}: Props) {
 
                         />
                         {/*  <div className='text-center text-slate-400 mt-1 uppercase'>Listing on Jan 5th!</div> */}
+                    </Link>
+                    <Link className="mt-0 mb-auto mx-auto h-36" href="https://traderjoexyz.com/avalanche/trade">
+                        <Image
+                            className='h-auto w-64 justify-self-top rounded-lg'
+                            src="/trader_joe.png"
+                            width={500}
+                            height={155}
+                            alt="trader joe logo"
+
+                        />
                     </Link>
 
                 </div>
